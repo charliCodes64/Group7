c53c170a83c21db4a4775e7256af20d8ec92ff3d
charlton64 <cinfinge@mail.umw.edu>
2025-04-15 13:38:00 -0400

    updated comments

--- Main.java
+++ Main.java
@@ -4,13 +4,20 @@ public class Main {
         // frame only needs to be called in here
     }
 }
+//The first line in the document is the room the individual starts in
+//The second line is the door that appears on the left "DoorL"
+//The third line is the door that appears on the right "DoorR"
+//After the ":" is the description for both the doors and rooms
+//The text inside "()" for the doors is for the action that is printed once they player chooses the door
+//The word "true" or "false" at the end of the DoorL or DoorR line indicates if it is the right door
+//If the door is false they cannot continue if the door is true they can
 //File format below is not the game only an example
 //Attended file format below
 
 //Room:You are in the first room of the maze
-//DoorL:A oak plank door appears (The Door catches fire once touched)'false'
-//DoorR:A purple door appears (A new room appears dark and damp)'true'
+//DoorL:oak plank door (The Door catches fire once touched)'false'
+//DoorR:purple door appears (A new room appears dark and damp)'true'
 //==========================================================================
 //Room:You have made it to the second room
-//DoorL:You see a stone door (A lonely cellar appears)'true'
-//DoorR:You see a glass door (The door shatters)'false'
+//DoorL:Stone door (A lonely cellar appears)'true'
+//DoorR:Glass door (The door shatters)'false'
