521ed9f7da715573ab25f443d7235285057cb554
brand <bbeiler@mail.umw.edu>
2025-04-22 20:41:03 -0400

    I added my name.

--- Doors.java
+++ Doors.java
@@ -1,9 +1,10 @@
+import java.io.PrintWriter;
 public class Doors {
 
     private String doorDescription;
     private String doorDirection;
     //below if door is 'false' in txt doc they should not be able to continue and must restart
-    private Boolean doorAccessibility;
+    private boolean doorAccessibility;
 
     Doors(String direction, String doorDescription, boolean doorAccessibility){
         this.doorDirection = direction;
@@ -14,7 +15,7 @@ public class Doors {
         return this.doorDirection;
     }
     public String getDescription(){
-        return this.doorDirection;
+        return this.doorDescription;
     }
     public boolean getDoorAccessibility(){
         boolean temp = true;
@@ -23,7 +24,12 @@ public class Doors {
 
     //below the method should make a new file and write which room the player failed at
     public void failedAttemptWriter(String failedRoomDescription){
-
+        File failedRoomFile = new File();
+        PrintWriter failedRoom = new PrintWriter(failedRoomFile);
+        if (doorAccessibility == false) {
+            System.out.println(failedRoomDescription);
+        }
+        failedRoom.print("You failed in Room " + doorDescription)
     }
     public String toString(){
         String temp = "temp";
